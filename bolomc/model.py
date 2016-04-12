53c79db24cdd30508ed93dc575344d1abcac7971
Danny Goldstein <dgold@berkeley.edu>
2016-04-12 14:26:25 -0700

    reimplement model as mcmc with 4 parameters and a warping surface

--- bolomc/model.py
+++ bolomc/model.py
@@ -9,11 +9,11 @@ import sncosmo
 import numpy as np
 
 from itertools import product
-from sklearn.gaussian_process import GaussianProcess
+from scipy.interpolate import RectBivariateSpline
 
 from burns import *
 from exceptions import *
-from distributions import TruncNorm
+from distributions import TruncNorm, stats
 
 ######################################################
 # CONSTANTS ##########################################
@@ -76,27 +76,24 @@ def compute_ratio(band, type='flux'):
     
     return numerator / denomenator
 
-#        t,  lambda
-TH_LO = [10**-2, 3e3**-2]
-TH_HI = [0.5**-2, 1e2**-2]
-TH_0  = [3**-2, 5e2**-2]
-
-NUG = 1e-5
 
 class FitContext(object):
 
-    """Implementation of the monte carlo model (Figure 1) from Goldstein &
-    Kasen (2016). Defines a set of priors and a likelihood function
-    for predicting bolometric light curves given broadband CSP
-    photometry.
+    """Implementation of the PGM (Figure 1) from Goldstein & Kasen
+    (2016). Defines a set of priors and a likelihood function for
+    predicting bolometric light curves given broadband CSP photometry.
 
     """
     
-    def __init__(self, lcfile, dust_type=sncosmo.OD94Dust, exclude_bands=[],
+    def __init__(self, lcfile, np, nl, dust_type=sncosmo.OD94Dust, exclude_bands=[],
                  rv_bintype='gmm'):
 
         self.dust_type = dust_type
         self.exclude_bands = exclude_bands
+        
+        self.np = np
+        self.nl = nl
+
         self.lc = sncosmo.read_lc(lcfile, format='csp')
         self.lc['wave_eff'] = map(filter_to_wave_eff, self.lc['filter'])
         self.lc.sort(['mjd', 'wave_eff'])
@@ -117,48 +114,77 @@ class FitContext(object):
         
         # Set up priors.
         
-        self.ebv_dist = TruncNorm(0., np.inf, self.host_ebv, self.host_ebv_err)
-        self.rv_dist  = get_hostrv_prior(self.lc.meta['name'],
-                                         self.rv_bintype,
-                                         self.dust_type)
+        self.ebv_prior = TruncNorm(0., np.inf, self.host_ebv, 
+                                   self.host_ebv_err)
 
-        # fit amplitude and t0 
-        model = self._create_model()
-        res, fitted_model = sncosmo.fit_lc(self.lc, model, ['amplitude','t0'])
+        self.rv_prior  = get_hostrv_prior(self.lc.meta['name'],
+                                          self.rv_bintype,
+                                          self.dust_type)
+
+        # set up coarse grid
+        self.xstar_p = np.linspace(self.hsiao._phase[0], 
+                                   self.hsiao._phase[1], 
+                                   self.np)
+        self.xstar_l = np.linspace(self.hsiao._wave[0], 
+                                   self.hsiao._wave[1], 
+                                   self.nl)
+        
+        # this is the coarse grid
+        self.xstar = np.asarray(list(product(self.xstar_p, 
+                                             self.xstar_l)))
+
+        # get an initial guess for amplitude and t0
+        
+        empty_arr = np.zeros(4 + self.np * self.nl)
+        guess_vec = ParamVec(empty_arr, self.np, self.nl)
+
+        guess_vec.ebv = self.ebv_prior.rvs()
+        guess_vec.rv = self.rv_prior.rvs()
+        
+        # harmless hack to avoid bounds errors
+        guess_vec.lt = 0.1
+        guess_vec.llam = 0.1
+
+        guess_mod = self._create_model(guess_vec)
+        
+        res, fitted_model = sncosmo.fit_lc(self.lc, guess_mod, ['amplitude','t0'])
         if not res['success']:
             raise FitError(res['message'])
 
+        # TODO:: check to see if this is still right
         self.amplitude = res['parameters'][2]
         self.t0 = res['parameters'][1]
-        self.gp = GaussianProcess(thetaL=TH_LO, 
-                                  thetaU=TH_HI, 
-                                  theta0=TH_0, 
-                                  nugget=NUG,
-                                  normalize=False)
 
+        # Deprecated
+
+        '''
         self.lc['mag'] = -2.5 * np.log10(self.lc['flux']) + self.lc['zp']
         self.lc['ms'] = map(magsys.standard_mag, self.lc['filter'])
+        ''' 
         
         self.hsiao_binw = np.gradient(self.hsiao._wave)
         
+        # Deprecated 
+
+        '''
         # compute the band ratio once for each band 
         self.ratio = {band.name : compute_ratio(band, type='photon') \
                       for band in self.bands}
 
         self.lc['ratio'] = [self.ratio[filt] for filt in self.lc['filter']]
+        '''
 
-        ### TODO:: Check this
         self.lc['mjd'] = self.lc['mjd'] - self.t0
-
         self.obs_x = np.asarray(zip(self.lc['mjd'], self.lc['wave_eff']))
-
-        # TODO:: Check this
         self.rest_x = self.obs_x / (1 + self.lc.meta['zcmb'])
 
-        self.gp_xstar = np.asarray(list(product(self.hsiao._phase,
-                                                self.hsiao._wave)))
+        # This is defined here and used repeatedly in the loglike
+        # calculation.
+        self.x = np.vstack((self.rest_x, self.xstar))
 
+        # Deprecated 
 
+        '''
         # quantities you only need to compute once
         self.numer = 10**(0.4 * (self.lc['ms'] - self.lc['mag'])) * \
                      self.lc['ratio'] * (1 + self.lc.meta['zcmb'])
@@ -168,11 +194,32 @@ class FitContext(object):
                                for tup in self.rest_x]) # monochromatic
                                                         # photon flux
 
-    def _create_model(self, source=None):
+        '''
+
+        
+
+    def _regrid_hsiao(self, warp_f):
+        """Take an SED warp matrix defined on a coarse grid and interpolate it
+        to the hsiao grid using a cubic spline.
+
+        """
+
+        spl = RectBivariateSpline(self.xstar_p, 
+                                  self.xstar_l,
+                                  warp_f)
+        
+        return spl(self.hsiao._phase,
+                   self.hsiao._wave)
+        
+                                                        
+    def _create_model(self, params):
         """If source is None, use Hsiao."""
-    
-        if source is None:
-            source = self.hsiao
+        
+        # warp the SED
+        flux = self._regrid_hsiao(params.sedw) * self.hsiao._passed_flux
+        source = sncosmo.TimeSeriesSource(self.hsiao._phase,
+                                          self.hsiao._wave,
+                                          flux)
         
         model = sncosmo.Model(source=source,
                               effects=[self.dust_type(), sncosmo.F99Dust()],
@@ -182,68 +229,79 @@ class FitContext(object):
         # spectroscopic redshift
         model.set(z=self.lc.meta['zcmb'])
         
-        # MW Rv is fixed at 3.1. 
+        # MW Rv is fixed at 3.1.  mwebv could be a parameter but we
+        # will fix it for simplicity
         model.set(mwebv=self.mwebv)
 
         # Draw random host reddening parameters. 
-        model.set(hostr_v=self.rv_dist.rvs())
-        model.set(hostebv=self.ebv_dist.rvs())
+        model.set(hostr_v=params.rv)
+        model.set(hostebv=params.ebv)
         
-        try:
-            model.set(amplitude=self.amplitude)
-        except AttributeError:
-            pass
-
-        try:
-            model.set(t0=self.t0)
-        except AttributeError:
-            pass
+        # set source parameters
+        # these are fixed
+        
+        model.set(amplitude=self.amplitude)
+        model.set(t0=self.t0)
 
         return model
 
-        
-    def mw_dust(self):
-        dust = sncosmo.F99Dust()
-        dust.set(ebv=self.mwebv)
-        return dust
+    def logprior(self, params):
+        """Compute logp(params)."""
 
-    def host_dust(self):
-        dust = self.dust_type()
-        dust.set(ebv=self.ebv_dist.rvs())
-        dust.set(r_v=self.rv_dist.rvs())
-        return dust
-
-    def one_iteration(self):
+        lp__ = 0
         
-        # Create dust
-        host_dust = self.host_dust()
-        mw_dust = self.mw_dust()
+        lp__ += self.ebv_prior(params.ebv)
+        lp__ += self.rv_prior(params.rv)
+
+        # do gaussian priors around the guesses for A and t0
+        
+        return lp__
+
+    def loglike(self, params):
+        """Compute loglike(params)."""
+        
+        lp__ = 0
+        model = self._create_model(params)
+        flux = model.bandflux(self.lc['filter'], 
+                              self.lc['mjd'])
+
+        # model / data likelihood calculation 
+        sqerr = ((flux - self.lc['flux']) / self.lc['fluxerr'])**2
+        lp__ += np.sum(sqerr)
+
+        # gaussian process likelihood calculation
+
+        ratio = flux / self.lc['flux']
+        ratio_m = ratio.mean()
+        ratio_sd = ratio.std()
+        ratio_scl = (ratio - ratio_m) / ratio_sd
+        sedw_scl = (params.sedw - ratio_m) / ratio_sd
+        y = np.concatenate((ratio_scl, sedw_scl.ravel()))
+
+        l = np.asarray([params.lp, params.llam])
+        sigma = (self.x[:, None] - self.x[None, :]) / l
+        sigma = np.exp(-np.sum(sigma * sigma), axis=-1)
+        sigma += np.diag(np.ones_like(y) * 1e-5)
+        mu = np.zeros_like(y)
+        
+        lp__ += stats.multivariate_normal.logpdf(y, mean=mu, cov=sigma)
+
+        return lp__
+
+    def bolo(self, params):
+        """Compute a bolometric flux curve for `params`."""
+        flux = self._regrid_hsiao(params.sedw) * self.hsiao._passed_flux
+        flux *= params.amplitude
+        return np.sum(flux * self.hsiao_binw, axis=1)
+
+    def __call__(self, params):
+        try:
+            vec = ParamVec(params, self.np, self.nl):
+        except BoundsError as e:
+            return -np.inf
+        return self.logprior(vec) + self.loglike(vec)
     
-        # Determine the warping at the knot points
-        D_R = host_dust.propagate(self.rest_x[:, 1], 1.) # second
-                                                         # argument is
-                                                         # a hack to
-                                                         # just get
-                                                         # the
-                                                         # transmission
-
-        D_O = mw_dust.propagate(self.obs_x[:, 1], 1.)    # ditto
-        
-        denom = D_R * self.S_R * D_O
-        
-        # try doing (numer / denom) - 1
-        warp = self.numer / denom
-
-        # train GP
-        self.gp.fit(self.rest_x, warp)
     
-        # interpolate
-        pf = self.hsiao._passed_flux
-        self.sedw = self.gp.predict(self.gp_xstar).reshape(pf.shape)
-
-        flux = self.amplitude * self.sedw * pf 
-        bolo = np.sum(flux * self.hsiao_binw, axis=1)
-        return bolo
 
 if __name__ == "__main__":
     
--- bolomc/vec.py
+++ bolomc/vec.py
@@ -5,7 +5,7 @@ __whatami__ = 'Parameter vector class for MCMC sampling.'
 from .exceptions import BoundsError
 
 class ParamVec(object):
-    
+        
     def __init__(self, vec, np, nl):
         self.vec = vec
         self._check_bounds()
@@ -15,6 +15,7 @@ class ParamVec(object):
     def _check_bounds(self):
         
         ermsg = '%s is out of bounds (%.4f, %.4f): %s'
+        inclermsg = '%s is out of bounds [%.4f, %.4f): %s'
         
         if self.lt <= 0:
             raise BoundsError(ermsg % ('lt', 0, np.inf, self.lt))
@@ -22,37 +23,47 @@ class ParamVec(object):
             raise BoundsError(ermsg % ('llam', 0, np.inf, self.llam))
         if self.rv <= 0:
             raise BoundsError(ermsg % ('rv', 0, np.inf, self.rv))
-        if self.amplitude <= 0:
-            raise BoundsError(ermsg % ('amplitude', 0, np.inf, self.amplitude))
-                
-    @property
-    def amplitude(self):
-        """Hsiao amplitude."""
-        return self.vec[0]
+        if self.amplitude < 0:
+            raise BoundsError(inclermsg % ('amplitude', 0, 
+                                           np.inf, self.amplitude))
 
-    @propety
-    def t0(self):
-        """Hsiao t0."""
-        return self.vec[1]
                 
     @property
     def lp(self):
-        return self.vec[2]
+        return self.vec[0]
+        
+    @lp.setter
+    def lp(self, x):
+        self.vec[0] = x
     
     @property
     def llam(self):
-        return self.vec[3]
+        return self.vec[1]
+
+    @llam.setter
+    def llam(self, x):
+        self.vec[1] = x
         
     @property
     def rv(self):
         # host
-        return self.vec[4]
+        return self.vec[2]
+        
+    @rv.setter
+    def rv(self, x):
+        self.vec[2] = x
         
     @property
     def ebv(self):
         # host
-        return self.vec[5]
+        return self.vec[3]
+        
+    @ebv.setter
+    def ebv(self, x):
+        self.vec[3] = x
     
     @property
-    def sed_warp(self):
-        return self.vec[6:].reshape(np, nl)
+    def sedw(self):
+        return self.vec[4:].reshape(np, nl)
+        
+    
