3f44226babef8a5054c401c07692ad9d89e456c3
Danny Goldstein <dgold@berkeley.edu>
2016-05-21 16:23:34 -0700

    add 2d sine test problem

---
+++ tests/sine.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+__author__ = "Danny Goldstein <dgold@berkeley.edu>"
+__whatami__ = "Test problem with a uniform warping surface."
+
+import matplotlib
+matplotlib.use("Agg")
+import sncosmo
+import problem
+import numpy as np
+
+def sedw(p, l):
+    return 1 + 0.5 * np.sin((np.pi * p / 20) + (np.pi * l / 1500)) 
+
+# True parameters.
+rv = 3.1 # Host.
+ebv = 0.1 # Host.
+lp = 20 # Days. TODO:: Verify this. 
+llam = 1500 # Angstrom. TODO:: Verify this.
+z = 0.02 
+mwebv = 0.02 
+
+test = problem.Problem(sedw, rv, ebv, lp, llam, z, mwebv)
+data = test.data(20, exclude_bands=['csphd','cspjd','cspyd',
+                                    'cspv3014','cspv3009'])
+
+fig = sncosmo.plot_lc(data=data)
