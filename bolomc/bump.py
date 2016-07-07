a2a39fe917c8e034f0ef5db5ba0485f294cea73a
Danny Goldstein <dgold@berkeley.edu>
2016-07-07 15:18:15 -0700

    add gmm

--- bolomc/bump.py
+++ bolomc/bump.py
@@ -8,7 +8,7 @@ import numpy as np
 from scipy.optimize import fmin_l_bfgs_b
 from scipy.interpolate import RectBivariateSpline as Spline2d
 from copy import copy
-
+from scipy.linalg import pinv
 
 class SmoothTophat(object):
     """A callable one-dimensional smooth tophat function with steepness
@@ -69,20 +69,6 @@ class Bump(object):
         wslice = 0 if wave.ndim == 0 else slice(None)
         return result[pslice, wslice]
 
-    def affects(self, phase, wave, nsigma=4):
-        """True if `phase` and `wave` are within the area of influence of the
-        kernel.
-
-        """
-        th_l = wave >= self._tophat.l
-        th_r = wave <= self._tophat.r
-        
-        gs_l = phase >= self._gaussian.mu - nsigma * self._gaussian.sigma
-        gs_r = phase <= self._gaussian.mu + nsigma * self._gaussian.sigma
-        
-        return th_l and th_r and gs_l and gs_r
-
-
     def _fit_kernel(self, source, guess=None):
 
         """Fit the `mu` and `sigma` parameters of the gaussian (phase)
@@ -121,6 +107,15 @@ class Bump(object):
             self._gaussian = Gaussian(*opt)
         else:
             raise RuntimeError(d['warnflag'])
+
+class HsiaoBump(Bump):
+    
+    def __init__(self, name):
+        hsiao = sncosmo.get_source('hsiao')
+        super(HsiaoBump, self).__init__(name, hsiao.minphase(),
+                                        hsiao.maxphase(), 
+                                        hsiao.minwave(),
+                                        hsiao.maxwave())
 
     
 class BumpSource(sncosmo.Source):
@@ -195,6 +190,94 @@ class BumpSource(sncosmo.Source):
                   bump.kernel(phase / self._parameters[1], wave))
         return f
     
+
+class FreeBumpSource(sncosmo.Source):
+    
+    def __init__(self, nbumps, name=None, version=None):
+        self.name = name
+        self.version = version
+        hsiao = sncosmo.get_source("hsiao", version='3.0')        
+        self._phase = hsiao._phase
+        self._wave = hsiao._wave
+        self._passed_flux = hsiao._passed_flux
+        self._param_names = ['amplitude', 's']
+        self.param_names_latex = ['A', 's']
+        self._parameters = np.array([1., 1.])
+        self._model_flux = Spline2d(self._phase, self._wave, 
+                                    self._passed_flux, kx=2, ky=2)
+        self.nbumps = nbumps
         
-                      
-                       
+        for i in range(self.nbumps):
+            self._parameters = np.concatenate((self._parameters, [0.] * 6))
+            self._param_names += map(lambda pref: pref + str(i), ['mu_phase_',
+                                                                  'mu_wave_',
+                                                                  'sig_phase_',
+                                                                  'sig_wave_',
+                                                                  'cov_',
+                                                                  'amp_'])
+
+            self.param_names_latex += map(lambda tok: tok % i, ['\mu_{p,%d}',
+                                                                '\mu_{\lambda, %d}',
+                                                                '\sigma_{p,%d}',
+                                                                '\sigma_{\lambda, %d}',
+                                                                '\sigma_{p \lambda, %d}',
+                                                                'A_%d'])
+
+    def minphase(self):
+        return self._parameters[1] * self._phase[0]
+
+    def maxphase(self):
+        return self._parameters[1] * self._phase[-1]
+        
+    def _unscale(self, scaled_params):
+        """scaled = A unscaled + B"""
+        upper = np.array([self._phase[-1], np.log10(self._wave[-1]),
+                          0.5 * (self._phase[-1] - self._phase[0]),
+                          0.5 * (np.log10(self._wave[-1]) - np.log10(self._wave[0])),
+                          1., 2.])
+        lower = np.array([self._phase[0], np.log10(self._wave[0]), 
+                          0., 0., 0, -2.])
+        
+        diff = upper - lower 
+        A = 2 / diff
+        B = 1 - (2 * upper / diff)
+        unscaled = (scaled_params - B) / A
+        unscaled[4] = unscaled[2] * unscaled[3] * scaled_params[4]
+        return unscaled
+        
+        
+    def _flux(self, phase, wave):
+        f = (self._parameters[0] *
+             self._model_flux(phase / self._parameters[1], wave))
+        
+        p = np.asarray(phase)
+        w = np.log10(np.asarray(wave))
+        
+        ndim_p = p.ndim
+        ndim_w = w.ndim
+        
+        p = np.atleast_1d(p)
+        w = np.atleast_1d(w)
+        
+        grid = np.asarray([[(pp, ww) for ww in w] for pp in p])
+        flatgrid = grid.reshape(-1, 2)
+        
+        pslice = 0 if ndim_p == 0 else slice(None)
+        wslice = 0 if ndim_w == 0 else slice(None)
+
+        for i in range(self.nbumps):
+            if i == self.nbumps - 1:
+                scaled_params = self.parameters[(-self.nbumps + i) * 6:]
+            else:
+                scaled_params = self.parameters[(-self.nbumps + i) * 6:(-self.nbumps + i + 1) * 6]
+            params = self._unscale(scaled_params)
+            cov = np.array([[params[2]**2, params[4]],[params[4], params[3]**2]])
+            invcov = pinv(cov)
+            mu = params[:2]
+            devs = flatgrid - mu
+            kern = np.asarray([np.exp(-row.dot(invcov).dot(row)) for row in devs]) * params[-1] 
+            kern = kern.reshape(p.shape + w.shape)
+            kern = kern[pslice, wslice]
+            f *= (1 + kern)
+
+        return f
