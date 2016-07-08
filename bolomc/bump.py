3cea5439d44e3508f68adbaee0f90c562517f0f2
Danny Goldstein <dgold@berkeley.edu>
2016-07-08 12:52:44 -0700

    make UV region bleed slightly into blue

--- bolomc/bump.py
+++ bolomc/bump.py
@@ -147,8 +147,8 @@ class BumpSource(sncosmo.Source):
         Must have shape `(num_phases, num_disp)`.
     """
 
-    BUMPS = [Bump('UV', 1000., 3000., -20, 20),
-             Bump('blue', 3000., 6900., -20, 20),
+    BUMPS = [Bump('UV', 1000., 3500., -20, 20),
+             Bump('blue', 3500., 6900., -20, 20),
              Bump('i1', 6900., 9000., -20, 15),
              Bump('i2', 6900., 9000., 15, 42),
              Bump('y1', 9000., 11200., -20, 13.6),
