d300022107c7db1b2f3e40966e72e541331e8707
Danny Goldstein <dgold@berkeley.edu>
2016-07-08 15:21:43 -0700

    fit everything

--- tests/test_bump.py
+++ tests/test_bump.py
@@ -5,50 +5,61 @@ import numpy as np
 from bolomc import bump
 from bolomc import burns
 from bolomc.distributions import TruncNorm
+import glob
 
-lc = sncosmo.read_lc('../data/CSP_Photometry_DR2/SN2005elopt+nir_photo.dat', format='csp')
+lc_files = glob.glob('../data/CSP_Photometry_DR2/*.dat')
+for lc_file in lc_files:
+    try:
 
-model = sncosmo.Model(bump.BumpSource(),
-                      effect_names=['host','mw'],
-                      effect_frames=['rest','obs'],
-                      effects=[sncosmo.OD94Dust(), sncosmo.F99Dust()])
+        lc = sncosmo.read_lc(lc_file, format='csp')
 
-rv_prior = burns.get_hostrv_prior(lc.meta['name'], 
-                                  'gmm', sncosmo.OD94Dust)
+        model = sncosmo.Model(bump.BumpSource(),
+                              effect_names=['host','mw'],
+                              effect_frames=['rest','obs'],
+                              effects=[sncosmo.OD94Dust(), sncosmo.F99Dust()])
 
-host_ebv, err = burns.get_hostebv(lc.meta['name'])
-ebv_prior = TruncNorm(-np.inf, np.inf, host_ebv, err)
+        rv_prior = burns.get_hostrv_prior(lc.meta['name'], 
+                                          'gmm', sncosmo.OD94Dust)
 
-model.set(z=lc.meta['zcmb'])
-model.set(mwebv=burns.get_mwebv(lc.meta['name'])[0])
-model.set(hostebv=host_ebv)
-model.set(hostr_v=rv_prior.mean)
-model.set(t0=burns.get_t0(lc.meta['name']))
+        host_ebv, err = burns.get_hostebv(lc.meta['name'])
+        ebv_prior = TruncNorm(-np.inf, np.inf, host_ebv, err)
 
-vparams = filter(lambda x: 'bump' in x, model._param_names)
-vparams += ['hostebv','hostr_v', 't0', 's']
-bounds = {b.name + "_bump_amp":(-1.5,1.5) for b in 
-                                 model.source.bumps}
-bounds['hostr_v'] = (0, 6.)
-bounds['hostebv'] = (0, 0.2)
+        model.set(z=lc.meta['zcmb'])
+        model.set(mwebv=burns.get_mwebv(lc.meta['name'])[0])
+        model.set(hostebv=host_ebv)
+        model.set(hostr_v=rv_prior.mean)
+        model.set(t0=burns.get_t0(lc.meta['name']))
 
-res, model = sncosmo.fit_lc(lc,model,['amplitude0', 'amplitude1']+vparams,
-                            bounds=bounds)
+        vparams = filter(lambda x: 'bump' in x, model._param_names)
+        vparams += ['hostebv','hostr_v', 't0', 's']
+        bounds = {b.name + "_bump_amp":(-1.5,1.5) for b in 
+                                         model.source.bumps}
+        bounds['hostr_v'] = (0, 6.)
+        bounds['hostebv'] = (0, 0.2)
 
-
-"""
-result = sncosmo.mcmc_lc(lc, model, vparams, priors={'hostebv':ebv_prior,
-                                                     'hostr_v':rv_prior},
-                         bounds=bounds,
-                         nwalkers=200,
-                         nburn=1,
-                         nsamples=20)
-"""
-
-                
+        res, model = sncosmo.fit_lc(lc,model,['amplitude0', 'amplitude1']+vparams,
+                                    bounds=bounds)
 
 
-#model.set(**dict(zip(vparams, ans)))
-#fig=sncosmo.plot_lc(data=lc, model=model)
-#fig.savefig('test.pdf')
+        """
+        result = sncosmo.mcmc_lc(lc, model, vparams, priors={'hostebv':ebv_prior,
+                                                             'hostr_v':rv_prior},
+                                 bounds=bounds,
+                                 nwalkers=200,
+                                 nburn=1,
+                                 nsamples=20)
+        """
 
+
+
+
+        #model.set(**dict(zip(vparams, ans)))
+        #fig=sncosmo.plot_lc(data=lc, model=model)
+        #fig.savefig('test.pdf')
+
+        fig = sncosmo.plot_lc(data=lc, model=model)
+        fig.savefig('fits/%s.pdf' % lc.meta['name'])
+        plt.close(fig)
+        print 'saved %s' % lc.meta['name']
+    except:
+        continue
