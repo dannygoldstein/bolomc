2c7ec5909aed423d74b0a3f2bcb07f514c1663aa
Danny Goldstein <dgold@berkeley.edu>
2016-08-05 16:14:57 -0500

    add wlr plot to book

--- book.py
+++ book.py
@@ -4,6 +4,7 @@ import sncosmo
 import glob
 import samples
 from matplotlib.backends.backend_pdf import PdfPages
+import matplotlib.pyplot as plt
 
 files = glob.glob('run/*.out')
 results = map(samples.models, files)
@@ -22,3 +23,18 @@ with PdfPages('bolo.pdf') as pdf:
         stack = bolo.LCStack.from_models(models, name=lc.meta['name'])
         ax = stack.plot()
         pdf.savefig(ax.figure)
+
+fig, ax = plt.subplots()
+dm15 = L = []
+dm15e = Le = []
+for (lc, config, models) in results:
+    tdm15 = map(bolo.dm15, models)
+    tL = map(bolo.Lpeak, models)
+    dm15.append(np.mean(tdm15))
+    L.append(np.mean(tL))
+    dm15e.append(np.std(tdm15))
+    Le.append(np.std(tL))
+ax.errorbar(dm15, L, xerr=dm15e, yerr=Le, fmt='.', capsize=0)
+fig.savefig('wlr.pdf')    
+
+
