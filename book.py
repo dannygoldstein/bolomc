2b62b53a47d50c3d9d93ed852483ca914dde0922
Danny Goldstein <dgold@berkeley.edu>
2016-09-13 13:50:59 -0700

    add convenience function for making allbolo plot to book

--- book.py
+++ book.py
@@ -6,29 +6,13 @@ import numpy as np
 import sncosmo
 import pickle
 from bolomc import bolo
+from copy import copy
 from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 from astropy.cosmology import Planck13
 from scipy.stats import multivariate_normal, spearmanr
 
-
-files = glob.glob('run/*.out')
-results = map(samples.models, files)
-csp = sncosmo.get_magsystem('csp')
-#fitres = np.genfromtxt('run/fitres.dat', names=True, dtype=None)
-
-# SNe that had fits that did not fail
-#good = fitres[fitres['status'] == 'OK']['name']
-
-# keep only successful fits
-#results = filter(lambda tup: tup[0].meta['name'] in good, 
-#                 results)
-
-# keep only SNe in the hubble flow
-results = filter(lambda tup: tup[0].meta['zhelio'] >= .01, results)
-
-names = [result[0].meta['name'] for result in results]
 
 def mc_spearmanr(x, y, cov, N=1000):
     mu = np.asarray(zip(x, y))
@@ -90,63 +74,112 @@ def wlr(x, y, cov, xlim=None, ylim=None, band='B'):
  
     return fig
 
+def plot_photmag(models, lc):
+    plt.rcParams["font.family"] = "monospace"
+    fig = sncosmo.plot_lc(model=models, data=lc,
+                          fill_percentiles=(2.5,50.,97.5),
+                          zpsys='csp', mag=True)
+    for axis in fig.axes:
+        yhi = axis.get_ylim()[1]
+        axis.set_ylim(23, yhi)
+        axis.tick_params(labelsize=8)
+        if axis.get_xlabel() != "":
+            axis.set_xlabel('mjd')
+    fig.set_size_inches(8., 6.)
+    fig.suptitle(lc.meta['name'])
+    fig.tight_layout()
+    fig.subplots_adjust(top=0.92, right=0.95)
+    return fig
 
-# broadband book
-with PdfPages('photmag.pdf') as pdf:
-    for (lc, config, models) in results:
-        fig = sncosmo.plot_lc(model=models, data=lc, fill_percentiles=(2.5, 50., 97.5), zpsys='csp',
-                              figtext=lc.meta['name'], mag=True)
-        pdf.savefig(fig)
-
-# bolometric book        
-with PdfPages('bolo.pdf') as pdf:
+def plot_all_bolometric_light_curves(models):
     from bolomc import bolo
+    fig, ax = plt.subplots()
+    for model_list in models:
+        stack = bolo.LCStack.from_models(model_list)
+        stack.plot(ax=ax)
+    ax.set_ylim(0, 2.5e43)
+    return fig
+    
+
+if __name__ == '__main__':    
+
+    files = glob.glob('run/*.out')
+    results = map(samples.models, files)
+    csp = sncosmo.get_magsystem('csp')
+    #fitres = np.genfromtxt('run/fitres.dat', names=True, dtype=None)
+
+    # SNe that had fits that did not fail
+    #good = fitres[fitres['status'] == 'OK']['name']
+
+    # keep only successful fits
+    #results = filter(lambda tup: tup[0].meta['name'] in good, 
+    #                 results)
+
+    # keep only SNe in the hubble flow
+    bad = np.genfromtxt('run/bad', dtype=None)
+    results = filter(lambda tup: tup[0].meta['zhelio'] >= .01, results)
+    results = filter(lambda tup: tup[0].meta['name'][2:] not in bad, results)
+
+    names = [result[0].meta['name'] for result in results]
+
+
+    """
+    # broadband book
+    with PdfPages('photmag.pdf') as pdf:
+        for (lc, config, models) in results:
+            fig = plot_photmag(models, lc)
+            pdf.savefig(fig)
+
+    # bolometric book        
+    with PdfPages('bolo.pdf') as pdf:
+        from bolomc import bolo
+        for (lc, config, models) in results:
+            stack = bolo.LCStack.from_models(models)
+            ax = stack.plot()
+            ax.set_title(lc.meta['name'])
+            ax.set_ylim(0, 2.5e43)
+            pdf.savefig(ax.figure)
+
+    """
+    # wlr plot
+    dm15 = []; M = []; cov = []
     for (lc, config, models) in results:
-        stack = bolo.LCStack.from_models(models)
-        ax = stack.plot()
-        ax.set_title(lc.meta['name'])
-        ax.set_ylim(0, 2.5e43)
-        pdf.savefig(ax.figure)
+        try: 
+            tdm15 = np.squeeze(map(bolo.dm15, models))
+            tL = np.squeeze(map(bolo.Lpeak, models))
+            # arbitrary zero point
+            tM = np.squeeze([-2.5 * np.log10(L) + 87.3 for L in tL])
 
-
-# wlr plot
-dm15 = []; M = []; cov = []
-for (lc, config, models) in results:
-    try: 
-        tdm15 = np.squeeze(map(bolo.dm15, models))
-        tL = np.squeeze(map(bolo.Lpeak, models))
-        # arbitrary zero point
-        tM = np.squeeze([-2.5 * np.log10(L) + 87.3 for L in tL])
-
-    except ValueError:
-        continue
-    dm15.append(np.mean(tdm15))
-    M.append(np.mean(tM))
-    cov.append(np.cov(zip(tdm15, tM), rowvar=False))
-print 'bolometric wlr spearman r: mean=%f, std=%f' % mc_spearmanr(dm15, M, cov)
-fig = wlr(dm15, M, cov, band='bol')
-fig.savefig('wlr.pdf')
-
-
-dm15 = []; M = []; cov = []
-for (lc, config, models) in results:
-    tdm15 = []
-    tM = []
-    for model in models:
-        peakmag = model.source_peakabsmag('cspb', csp, cosmo=Planck13)
-        peakphase = model.source.peakphase('cspb')
-        my_dm15 = model.source.bandmag('cspb', csp, peakphase+15) - model.source.bandmag('cspb', csp, peakphase)
-        #tp = model.get('t0') + (1 + model.get('z')) * peakphase
-        #mag0 = model.bandmag('cspb', csp, tp)
-        #mag15 = model.bandmag('cspb', csp, tp+15)
-        tdm15.append(my_dm15)
-        tM.append(peakmag)
-    if np.mean(tdm15) < 1.6:
+        except ValueError:
+            continue
         dm15.append(np.mean(tdm15))
         M.append(np.mean(tM))
         cov.append(np.cov(zip(tdm15, tM), rowvar=False))
+    print 'bolometric wlr spearman r: mean=%f, std=%f' % mc_spearmanr(dm15, M, cov)
+    fig = wlr(dm15, M, cov, band='bol')
+    fig.savefig('wlr.pdf')
 
-print 'b band wlr spearman r: mean=%f, std=%f' % mc_spearmanr(dm15, M, cov)
-fig = wlr(dm15, M, cov)# xlim=(.75, 1.75), 
-          #ylim=(-19.7, -18.4))
-fig.savefig('bbwlr.pdf')
+
+    dm15 = []; M = []; cov = []
+    for (lc, config, models) in results:
+        tdm15 = []
+        tM = []
+        for model in models:
+            peakmag = model.source_peakabsmag('cspb', csp, cosmo=Planck13)
+            peakphase = model.source.peakphase('cspb')
+            my_dm15 = model.source.bandmag('cspb', csp, peakphase+15) - model.source.bandmag('cspb', csp, peakphase)
+            #tp = model.get('t0') + (1 + model.get('z')) * peakphase
+            #mag0 = model.bandmag('cspb', csp, tp)
+            #mag15 = model.bandmag('cspb', csp, tp+15)
+            tdm15.append(my_dm15)
+            tM.append(peakmag)
+        if np.mean(tdm15) < 1.6:
+            dm15.append(np.mean(tdm15))
+            M.append(np.mean(tM))
+            cov.append(np.cov(zip(tdm15, tM), rowvar=False))
+
+    print 'b band wlr spearman r: mean=%f, std=%f' % mc_spearmanr(dm15, M, cov)
+    fig = wlr(dm15, M, cov)# xlim=(.75, 1.75), 
+              #ylim=(-19.7, -18.4))
+    fig.savefig('bbwlr.pdf')
+
